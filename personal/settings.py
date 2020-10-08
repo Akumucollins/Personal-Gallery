5679d55ae9c38ce99e60b12a5288283fdb1f29b5
Akumucollins <akumucollins@gmail.com>
2020-10-08 17:44:48 +0300

    craeted a route, add bootstrap files and folders, created a head.html,index.html and scripts.html

--- personal/settings.py
+++ personal/settings.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -108,7 +109,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 # Extra places for collectstatic to find static files.
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+STATICFILES_DIRS = [BASE_DIR, 'static']
 
