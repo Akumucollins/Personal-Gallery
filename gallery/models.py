f52f0e6633a5bbe6bf5ba6ac00f8c2251d04c262
Akumucollins <akumucollins@gmail.com>
2020-10-11 23:31:13 +0300

    created a image_category table, added a category route,created a loop for image_category

--- gallery/models.py
+++ gallery/models.py
@@ -40,7 +40,7 @@ class Image(models.Model):
     author = models.CharField(max_length=30,null=False,blank=False)
     timestamp = models.DateTimeField(auto_now_add=True,auto_now=False)
     update = models.DateTimeField(auto_now_add=False,auto_now=True)
-    category = models.ManyToManyField(Category) 
+    category = models.ForeignKey(Category,on_delete=models.CASCADE) 
     location = models.ForeignKey(Location,on_delete=models.CASCADE,default=1)  
     
 
--- gallery/views.py
+++ gallery/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
-from .models import Image,Location
+from .models import *
 
 def home(request):
      images = Image.objects.all()
+     category = Category.objects.all()
      locations = Location.get_location()
-     return render(request, "index.html",{'images':images,'locations':locations})
+
+     return render(request,"index.html",{'images':images,'locations':locations,'category':category})
 
 def location_img(request, location):
     images = Image.filter_by_location(location)
