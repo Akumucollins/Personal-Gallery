8e7445f45ffa9f3189f5024f4557ba25367e1546
Akumucollins <akumucollins@gmail.com>
2020-10-12 01:32:15 +0300

    added images and a location urlpattern

--- gallery/urls.py
+++ gallery/urls.py
@@ -4,8 +4,10 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 app_name = 'gallery'
+
 urlpatterns=[
   url(r'^$', views.home, name='home'),
+  url(r'^location/(?P<location>\w+)/', views.location_img, name='location'),
 ] + static(
             settings.MEDIA_URL, 
             document_root = settings.MEDIA_ROOT
