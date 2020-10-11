b977e1086720b5c499ec073980aeb2c66ed490d9
Akumucollins <akumucollins@gmail.com>
2020-10-12 00:09:21 +0300

    created a route to display image_location, created a category.html for view function

--- gallery/views.py
+++ gallery/views.py
@@ -6,6 +6,15 @@ def home(request):
      category = Category.objects.all()
      locations = Location.get_location()
 
+     if 'location' in request.GET and request.GET['location']:
+        name = request.GET.get('location')
+        images = Image.view_location(name)
+
+     elif 'category' in request.GET and request.GET['category']:
+        cat = request.GET.get('Category')
+        images = Image.view_category(cat)
+
+        return render(request, 'index.html', {"name":name,"images":images,"cat":cat })
      return render(request,"index.html",{'images':images,'locations':locations,'category':category})
 
 def location_img(request, location):
